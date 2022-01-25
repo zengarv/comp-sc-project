ceb2b40bdfe008d5e3e6ea1522832091da0417e1
zengarv <56085798+zengarv@users.noreply.github.com>
2022-01-25 09:07:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import time
-
-t0 = time.time()
 import requests
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 from fpdf import FPDF
 from datetime import datetime
 import os
-
 
 # Get HTML source code
 
@@ -29,7 +25,6 @@ def graph():
     dates = confirm = active = death = cure = count = []
     grp_list = [dates, confirm, active, death, cure, count]
     var_list = var_segment.strip().split('\n')
-    
     for index in range(len(var_list)):
         grp_list[index] = var_list[index].lstrip(' var').lstrip('abcdefghijklmnopqrstuvwxyz ').lstrip(' =[').rstrip(
             '"]; ').split('","' if index == 0 else ',')
@@ -63,7 +58,6 @@ def graph():
         plt.title('Last 14 Days')
         plt.legend(loc="upper left")
         plt.savefig('assets\\last14daystat.jpeg', bbox_inches='tight')
-        plt.close()
 
     def statewise_stat():
         statestaturl = 'https://www.ndtv.com/coronavirus/india-covid-19-tracker'
@@ -92,6 +86,8 @@ def graph():
         states_list.pop(len(states_list) - 1)
         states_list.pop(len(states_list) - 1)
 
+        fig = plt.figure(figsize=(10, 5))
+
         # creating the bar plot
         plt.bar(states_list, st_cases, label='Cases')
         plt.bar(states_list, st_active, label='Active')
@@ -104,12 +100,10 @@ def graph():
         plt.title("Statewise Demographics")
         plt.legend(loc="upper right")
         plt.savefig('assets\\statewisestat.jpeg', bbox_inches='tight')
-        plt.close()
 
     overall_stat()
     last14day()
     statewise_stat()
-
 
 def get_articles():
     url = 'https://www.google.com/search?q=covid+19&oq=covid+19&aqs=chrome..69i57j69i59l4j69i60.1448j0j9&sourceid=chrome&ie=UTF-8#wptab=s:H4sIAAAAAAAAAONgVuLVT9c3NMwySk6OL8zJecRowS3w8sc9YSn9SWtOXmPU5OIKzsgvd80rySypFJLmYoOyBKX4uVB18uxi4vZITcwpyQguSSwpXsQqmJxflJ-XWJZZVFqsUAwSAwD24HAsbgAAAA'
@@ -120,10 +114,27 @@ def get_articles():
     global fax
     fax = []
     for stuff in fact:
-        fax.append(stuff.get_text().replace('\n', '').replace('’', "'").replace('‘', "'"))
+        fax.append(stuff.get_text().replace('\n', '').replace('’', "'"))
     fax.pop(len(fax) - 1)
     fax.pop(0)
 
+def get_vaccination_details():
+    url = 'https://www.google.com/search?q=covid+vaccination&oq=covid+vaccination&aqs=chrome..69i57j69i60l3.3281j0j1&sourceid=chrome&ie=UTF-8'
+
+    d = requests.get(url)
+    soup = BeautifulSoup(d.text, "html.parser")
+
+    stuff = soup.findAll("div", class_="BNeawe s3v9rd AP7Wnd")
+
+    for s in stuff:
+        if "Vaccination Coverage" in s.text:
+            vaccination_coverage_list = s.text.split('.')
+            try:
+                int(''.join(vaccination_coverage_list[2].strip().split(',')))
+                todays_vaccination, total_vaccinations = vaccination_coverage_list[2], vaccination_coverage_list[4]
+            except:
+                todays_vaccination, total_vaccinations = 'Not Available', 'Not Available'
+            return todays_vaccination, total_vaccinations
 
 graph()
 get_articles()
@@ -132,16 +143,13 @@ now = datetime.now()
 title = 'The Covid Bugle'
 pdf_name = f'The Covid Bugle, {now.strftime("%B %d, %Y")}.pdf'
 
-
+todays_vaccinations, total_vaccination_data = get_vaccination_details()
 class PDF(FPDF):
-    
     def header(self):
-        
         self.set_font('times', 'B', 16)
         self.cell(70, align='C')
         title_w = self.get_string_width(title) + 6
         doc_w = self.w
-        
         self.set_x((doc_w - title_w) / 2)
         self.set_draw_color(0, 80, 180)  # Border Colour: blue
         self.set_fill_color(230, 230, 0)  # Background Color of Title: yellow
@@ -149,6 +157,7 @@ class PDF(FPDF):
         self.set_line_width(1)
         self.cell(title_w, 10, title, border=True, ln=1, align='C', fill=True)
 
+        now = datetime.now()
         self.set_font('courier', '', 7)
         self.cell(190, 7, f'Generated on {now.strftime("%b %d, %Y at %H:%M")}', align='C')
         self.image('assets\\covpic.png', 10, 2, 20)
@@ -166,7 +175,6 @@ class PDF(FPDF):
 
 
 def generate_pdf():
-    
     # Setup
     pdf = PDF('P', 'mm', 'A4')
     pdf.set_auto_page_break(auto=True, margin=15)
@@ -184,15 +192,28 @@ def generate_pdf():
     pdf.cell(0, 160, 'Top Stories')
     pdf.set_font('helvetica', '', 10)
     pdf.set_y(115)
-    
     for articles in fax:
         articles = articles.encode('latin-1', 'replace').decode('latin-1')
-        pdf.multi_cell(w=180, h=6, txt='- ' + articles, ln=True, border=False)
+        pdf.multi_cell(w=180, h=6, txt='- '+articles, ln=True, border=False)
+    
+    pdf.add_page()
+    pdf.set_font('times', 'BU', 20)
+    pdf.cell(0, 10, 'Vaccination Data')
+
+    pdf.set_y(30)
+    pdf.set_font('helvetica', '', 10)
+    pdf.cell(0, 30, f'Total Vaccinations: {total_vaccination_data}')
+
+    pdf.set_y(30)
+    pdf.set_font('helvetica', '', 10)
+    pdf.cell(0, 40, f'Today\'s Vaccinations: {todays_vaccinations}')
+
 
     # Saving
     pdf.output(pdf_name)
     print(f'PDF generated at {os.getcwd()}\\{pdf_name}')
 
+    # Deleting all graphs
     for img in os.listdir('assets'):
         if img not in ['covpic.png']:
             os.remove(f'assets\\{img}')
